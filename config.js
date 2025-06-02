9f3b1d0e115c830b227e3590995b8db546babf28
Solace <monkdev9@gmail.com>
2025-06-02 21:43:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
 module.exports = {
-    prefix: "!"
+    PREFIX: '!',
+    LOG_CHANNEL_NAME: 'mod-logs',
+    SUCCESS_COLOR: 0x00ff00,
+    ERROR_COLOR: 0xff0000,
+    WARNING_COLOR: 0xffff00,
+    INFO_COLOR: 0x00ccff
 };
